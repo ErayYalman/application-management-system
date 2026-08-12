516498ea6b606d87a2b2f8d01903e42e2ac3569b
eray yalman <erayyalman123@gmail.com>
2026-08-12 16:51:13 +0300

    fix: AplicationReportController applications is param

--- backend/application-management/src/main/java/com/cybersoft/application_management/controller/ApplicationReportController.java
+++ backend/application-management/src/main/java/com/cybersoft/application_management/controller/ApplicationReportController.java
@@ -1,13 +1,18 @@
 package com.cybersoft.application_management.controller;
 
+import java.time.Instant;
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cybersoft.application_management.dto.request.ApplicationReportRequest;
 import com.cybersoft.application_management.dto.response.ApplicationReportResponse;
+import com.cybersoft.application_management.enums.ApplicationStatus;
 import com.cybersoft.application_management.service.ApplicationReportService;
 
 import lombok.RequiredArgsConstructor;
@@ -22,11 +27,20 @@ public class ApplicationReportController {
     @GetMapping("/applications")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ApplicationReportResponse> getApplicationReport(
-            ApplicationReportRequest request) {
-                
+            @RequestParam(required = false) Instant startDate,
+            @RequestParam(required = false) Instant endDate,
+            @RequestParam(required = false) ApplicationStatus status,
+            @RequestParam(required = false) UUID formTypeId) {
+
+        ApplicationReportRequest request = ApplicationReportRequest.builder()
+                .startDate(startDate)
+                .endDate(endDate)
+                .status(status)
+                .formTypeId(formTypeId)
+                .build();
+
         return ResponseEntity.ok(
-                applicationReportService.getApplicationReport(
-                        request));
+                applicationReportService.getApplicationReport(request));
     }
 
 }
